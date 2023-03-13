52efc0078af87acd412947edd645e8a3777a78c0
Kelly Castelo <kawokkun@gmail.com>
2023-03-13 11:14:39 -0300

    add routes to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,37 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
-const routes: Routes = [{ path: 'about', component: AboutComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'wishlist', component: WishlistComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, AboutComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    ShopComponent,
+    CartComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    WishlistComponent,
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [],
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toRegister(): void {
+    this.router.navigate(['/register']);
   }
 
+  toCart(): void {
+    this.router.navigate(['/cart']);
+  }
+
+  toWishList(): void {
+    this.router.navigate(['/wishlist']);
+  }
 }
